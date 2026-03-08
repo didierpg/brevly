42ae239237c10d26f56cee03da40a76e9d18de6d
Didier Ferreira <didierpg@gmail.com>
2026-03-08 20:45:46 -0300

    feat: add Either to infra

---
+++ server/src/infra/shared/either.ts
@@ -0,0 +1,28 @@
+export type Left<T> = {
+  left: T;
+  right?: never;
+};
+
+export type Right<U> = {
+  left?: never;
+  right: U;
+};
+
+export type Either<T, U> = NonNullable<Left<T> | Right<U>>;
+
+export const isLeft = <T, U>(e: Either<T, U>): e is Left<T> => {
+  return e.left !== undefined;
+};
+
+export const isRight = <T, U>(e: Either<T, U>): e is Right<U> => {
+  return e.right !== undefined;
+};
+
+export const makeLeft = <T>(value: T): Left<T> => ({ left: value });
+
+export const makeRight = <U>(value: U): Right<U> => ({ right: value });
+
+export const unwrapEither = <T, U>(e: Either<T, U>): T | U => {
+  if (e.left !== undefined) return e.left;
+  return e.right as U;
+};
