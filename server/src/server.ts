a60f28435cd895e4bd00249d410eda5a30b2da51
Didier Ferreira <didierpg@gmail.com>
2026-03-08 21:29:36 -0300

    feat: add swagger

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,32 @@ import { env } from "@/env";
 import fastify from "fastify";
 import "dotenv/config";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Brev.ly API",
+      description: "API do Encurtador de URLs da Pós-Graduação",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.listen({ port: env.PORT }, (err, address) => {
   if (err) {
