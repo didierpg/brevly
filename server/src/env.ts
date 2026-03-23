2ce586c56d52b288a068c99b4de71ef0f40b434b
Didier Ferreira <didierpg@gmail.com>
2026-03-23 14:48:38 -0300

    feat(infra): setup postgres service with persistent volumes and healthcheck

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,6 +3,10 @@ import { z } from "zod";
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_HOST: z.string().default("localhost"),
+  POSTGRES_DB: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
